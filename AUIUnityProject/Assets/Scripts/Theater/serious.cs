c5ddfbd9a134318dfa1d485c9e5ca88ed07aaaf5
zeri91 <l.salerno91@gmail.com>
2018-12-12 04:32:02 +0100

    animators fixed, script for serious model added

---
+++ AUIUnityProject/Assets/Scripts/Theater/serious.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class serious : MonoBehaviour {
+
+    Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.S)) {
+            float r = Random.value;
+            animator.SetFloat("rand", r);
+        }
+    }
+}
